19ecd685b913bb5b36c7464418ffce8ae079ea10
Bogdan Popa <bogdan@defn.io>
2019-09-24 15:46:51 +0300

    core: add win32 i386 dep

--- sass/info.rkt
+++ sass/info.rkt
@@ -3,6 +3,7 @@
 (define version "0.0.0")
 (define collection "sass")
 (define deps '("base"
+               ("libsass-i386-win32" #:platform "win32\\i386")
                ("libsass-x86_64-linux" #:platform "x86_64-linux")
                ("libsass-x86_64-macosx" #:platform "x86_64-macosx")
                ("libsass-x86_64-win32" #:platform "win32\\x86_64")))
