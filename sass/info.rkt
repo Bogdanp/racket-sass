66193f604e58cf75d5613e5b9127a9eb854fa268
Bogdan Popa <bogdan@defn.io>
2019-09-28 19:55:49 +0300

    core: fixup platform declarations for libsass

--- sass/info.rkt
+++ sass/info.rkt
@@ -3,10 +3,10 @@
 (define version "0.0.0")
 (define collection "sass")
 (define deps '("base"
-               ("libsass-i386-win32" #:platform "win32\\i386")
-               ("libsass-x86_64-linux" #:platform "x86_64-linux")
-               ("libsass-x86_64-macosx" #:platform "x86_64-macosx")
-               ("libsass-x86_64-win32" #:platform "win32\\x86_64")))
+               ("libsass-i386-win32" #:platform #rx"win32\\i386")
+               ("libsass-x86_64-linux" #:platform #rx"x86_64-linux")
+               ("libsass-x86_64-macosx" #:platform #rx"x86_64-macosx")
+               ("libsass-x86_64-win32" #:platform #rx"win32\\x86_64")))
 (define build-deps '("racket-doc"
                      "rackunit-lib"
                      "scribble-lib"))
