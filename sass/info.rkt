8abb33d2329872ea162bc5fd22fcc4ea159c9d0c
Bogdan Popa <bogdan@defn.io>
2019-10-07 09:43:07 +0300

    build: add libsass to build deps
    Maybe this'll finally make the tests on pkg-build pass?

--- sass/info.rkt
+++ sass/info.rkt
@@ -9,5 +9,9 @@
                ("libsass-x86_64-win32" #:platform #rx"win32\\x86_64")))
 (define build-deps '("racket-doc"
                      "rackunit-lib"
-                     "scribble-lib"))
+                     "scribble-lib"
+                     ("libsass-i386-win32" #:platform #rx"win32\\i386")
+                     ("libsass-x86_64-linux" #:platform #rx"x86_64-linux")
+                     ("libsass-x86_64-macosx" #:platform #rx"x86_64-macosx")
+                     ("libsass-x86_64-win32" #:platform #rx"win32\\x86_64")))
 (define scribblings '(("sass.scrbl")))
