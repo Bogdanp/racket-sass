377f35b70cde9e239b1e08ad25e432eb7ab58bb7
Bogdan Popa <bogdan@defn.io>
2020-02-21 11:40:20 +0200

    win32: fix platform specifiers

--- sass/info.rkt
+++ sass/info.rkt
@@ -3,15 +3,15 @@
 (define version "0.0.0")
 (define collection "sass")
 (define deps '("base"
-               ("libsass-i386-win32" #:platform #rx"win32\\i386")
+               ("libsass-i386-win32" #:platform #rx"win32.i386")
                ("libsass-x86_64-linux" #:platform #rx"x86_64-linux")
                ("libsass-x86_64-macosx" #:platform #rx"x86_64-macosx")
-               ("libsass-x86_64-win32" #:platform #rx"win32\\x86_64")))
+               ("libsass-x86_64-win32" #:platform #rx"win32.x86_64")))
 (define build-deps '("racket-doc"
                      "rackunit-lib"
                      "scribble-lib"
-                     ("libsass-i386-win32" #:platform #rx"win32\\i386")
+                     ("libsass-i386-win32" #:platform #rx"win32.i386")
                      ("libsass-x86_64-linux" #:platform #rx"x86_64-linux")
                      ("libsass-x86_64-macosx" #:platform #rx"x86_64-macosx")
-                     ("libsass-x86_64-win32" #:platform #rx"win32\\x86_64")))
+                     ("libsass-x86_64-win32" #:platform #rx"win32.x86_64")))
 (define scribblings '(("sass.scrbl")))
